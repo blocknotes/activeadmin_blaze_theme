9f9ad71917665edb5cd02af9555572b75b2c3e29
Mattia Roccoberton <mroccoberton@DD-005.local>
2017-09-24 23:26:02 +0200

    Added index action buttons icons

--- lib/activeadmin_blaze_theme.rb
+++ lib/activeadmin_blaze_theme.rb
@@ -9,3 +9,20 @@ module ActiveAdminBlazeTheme
     end
   end
 end
+
+ActiveAdmin::Views::IndexAsTable::IndexTableFor::TableActions.class_eval do
+  def item *args
+    cl = args[2][:class]
+    if cl.include? 'view_link'
+      args[0] = '<span class="icon-eye"></span> '.html_safe + args[0]
+      args[2][:class] += ' c-button c-button--ghost-info u-xsmall'
+    elsif cl.include? 'edit_link'
+      args[0] = '<span class="icon-pencil"></span> '.html_safe + args[0]
+      args[2][:class] += ' c-button c-button--ghost-info u-xsmall'
+    elsif cl.include? 'delete_link'
+      args[0] = '<span class="icon-bin"></span> '.html_safe + args[0]
+      args[2][:class] += ' c-button c-button--ghost-error u-xsmall'
+    end
+    text_node link_to *args
+  end
+end
--- lib/activeadmin_blaze_theme/version.rb
+++ lib/activeadmin_blaze_theme/version.rb
@@ -1,3 +1,3 @@
 module ActiveAdminBlazeTheme
-  VERSION = '0.5.6'
+  VERSION = '0.5.8'
 end
